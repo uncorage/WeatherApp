64046afd0aee6489894e38004bcb8c820f9c8302
Jumayev-A <abdy.jumayev@gmail.com>
2023-01-22 11:35:50 +0500

    fir

--- WeaatherApp/apps/main/views.py
+++ WeaatherApp/apps/main/views.py
@@ -1,28 +1,35 @@
 from django.shortcuts import render
 import requests
 from bs4 import BeautifulSoup
+from django.http import HttpResponse
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
 
 def index(request):
     if request.method == 'POST':
-        city = request.replace(" ","+")
+        print(request.POST)
+        city = request.POST['city'].replace(" ","")
+        city = city+" Weathers"
+        print(city)
         response = requests.get(f'https://www.google.com/search?q={city}&oq={city}&aqs=chrome.0.35i39l2j0l4j46j69i60.6128j1j7&sourceid=chrome&ie=UTF-8',headers=headers)
+        # print(response.content)
         soup = BeautifulSoup(response.text,'html.parser')
+        location = soup.select('#wob_loc')[0].getText().strip() 
+        time = soup.select('#wob_dts')[0].getText().strip(),
+        info = soup.select('#wob_dc')[0].getText().strip()
+        weather = soup.select('#wob_tm')[0].getText().strip()
 
         data = {
-            'location': soup.select('#wob_loc')[0].getText().strip(),  
-            'time': soup.select('#wob_dts')[0].getText().strip(),
-            'info': soup.select('#wob_dc')[0].getText().strip(),
-            'weather': soup.select('#wob_tm')[0].getText().strip(),
+            'location': location,  
+            'time': time,
+            'info': info,
+            'weather': f"{weather}C",
         }
+        print(data)
 
-        return(data['location'])
-        return(data['time'])
-        return(data['info'])
-        return(data['weather']+"°C") 
+        # return HttpResponse(data)
     
     else:
         data = {}
             
-        return render(request, "main/index.html", data)
+    return render(request, "main/index.html", data)
